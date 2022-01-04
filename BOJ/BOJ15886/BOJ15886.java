ca16d24482d078c8dec0a584a313a0c98c61aa2b
김영훈 <44612896+HoonyCode@users.noreply.github.com>
2022-01-04 17:04:30 +0900

    Add files via upload

---
+++ BOJ/BOJ15886/BOJ15886.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ15886 {
+
+    static boolean[] v;
+    static int answer = 0;
+    static char[] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        map = br.readLine().toCharArray();
+        v = new boolean[N];
+
+        for (int i = 0; i < N; i++){
+            if (v[i]) continue;
+            bfs(i);
+        }
+        System.out.println(answer);
+    }
+
+    private static void bfs(int start) {
+        v[start] = true;
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(start);
+
+        int cur;
+        int next;
+        while (!que.isEmpty()){
+            cur = que.poll();
+            next = cur + (map[cur] == 'E' ? 1 : -1);
+
+            if (map[cur] != map[next]){
+                v[next] = true;
+                break;
+            }
+            if (v[next]){
+                return;
+            }
+            que.offer(next);
+            v[next] = true;
+        }
+
+        answer++;
+    }
+}
