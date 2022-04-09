8dbf63a2cf2065bf87f641765a6304e65ef0e5dd
hoonyCode <rladudgns456@naver.com>
2022-04-09 21:25:05 +0900

    solve Algorithm

---
+++ BOJ/BOJ_11652.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class BOJ_11652 {
+
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<Long, Integer> map = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        Long temp;
+        for (int i = 0 ; i < N ; i++){
+            temp = Long.parseLong(br.readLine());
+
+            if (map.containsKey(temp)){
+                map.replace(temp, map.get(temp) + 1);
+            }else {
+                map.put(temp,1);
+            }
+        }
+
+        Long answer = Long.MAX_VALUE;
+        int cnt = 0;
+
+        for (Map.Entry<Long, Integer> cur : map.entrySet()) {
+            Long key = cur.getKey();
+            Integer value = cur.getValue();
+
+            if (value >= cnt){
+                if (cnt == value){
+                    answer = Math.min(answer, key);
+                }else {
+                    answer = key;
+                }
+                cnt = value;
+            }
+        }
+
+        System.out.print(answer);
+
+    }
+}
