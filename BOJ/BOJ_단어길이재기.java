ad69c2cc522e6719893be139dcb2bf76bc4aebf0
hoonyCode <rladudgns456@naver.com>
2022-05-10 19:52:11 +0900

    solve BOJ_단어길이재기

---
+++ BOJ/BOJ_단어길이재기.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_단어길이재기 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int answer = br.readLine().length();
+
+        System.out.println(answer);
+    }
+}
